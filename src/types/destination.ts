2a3fe61d164bcdd2709b72e17fea64158da1eb1f
HananMuhammad <jasminis1411@gmail.com>
2026-08-23 21:09:12 +0530

    Add Destination interface for type definitions

---
+++ src/types/destination.ts
@@ -0,0 +1,10 @@
+export interface Destination {
+  id: string;
+  name: string;
+  country: string;
+  description: string;
+  pricePerNight: number;
+  currency: string;
+  imageAlt: string;
+  verified: boolean;
+}
